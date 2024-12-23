229020111113e06602ef5f7ac03a5eb5666b9671
Dead Paul <122644231+Dead-Paul@users.noreply.github.com>
2024-12-23 15:59:01 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,32 @@
-const addImg = function () {
-    const qrImg = document.createElement('img');
-    qrImg.src = './img/test.png';
-    document.body.appendChild(qrImg);
-}
+const displayError = (text) => {
+    h1 = document.getElementsByTagName('h1')[0];
+    h1.textContent = text;
+    h1.style.color = 'red';
+};
 
-document.addEventListener('DOMContentLoaded', addImg);
+const displayJarInfo = async function () {
+    try {
+        let info = await fetch('https://api.monobank.ua/personal/client-info',
+            {
+                method: 'GET',
+                headers: {
+                    'X-Token': 'uCmgOU8-m487ZsYzS9IRyiF1oi0AVgojEt_QTpSDyTiI'
+                }
+            }
+        );
+        info = await info.json();
+        jarInfo = info.jars[0];
+        if (jarInfo) {
+            document.getElementById('collected-money').textContent = `Собрано: ${jarInfo.balance / 100}`;
+            document.getElementById('target-money').textContent = `Цель: ${jarInfo.goal / 100}`;
+            document.getElementById('link-to-send').href = 'https://send.monobank.ua/' + jarInfo.sendId;    
+        }
+        else {
+            displayError('Не удалось найти накопления')
+        }
+    } catch (error) {
+        displayError('Не удалось получить накопления')
+    }
+};
+
+document.addEventListener('DOMContentLoaded', displayJarInfo);
