3d74d30adec5711cddd0f5b15014184159477700
Dead Paul <dead.every.side@gmail.com>
2025-01-06 17:17:58 +0100

    Updated submodule custom-anime-chan

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
 //@ts-check
 
+import {AnimeChan, setAnimeChanEvents} from '../custom-anime-chan/AnimeChan.js'
+
+/** Path for all img sources @type {String}*/
+const path = './img/anime-chan/';
+
+/** Anime character @type {AnimeChan}*/
+const animeChan = new AnimeChan('anime-chan-container', {
+    sclera: { main: { src: path + 'sclera.png', position: { x: 0, y: 0 } } },
+    eyes: {
+        main: { src: path + 'eyes.png', position: { x: 595, y: 368 } },
+        //@ts-ignore : There is no requirement to write this objects in the constructor, but it will not throw error
+        love: { src: path + 'eyes-love.png', position: { x: 595, y: 368 }},
+        think: { src: path + 'eyes-think.png', position: { x: 595, y: 368 }}
+    },
+    body: { main: { src: path + 'body.png', position: { x: 0, y: 0 } } },
+    mouth: {
+        main: { src: path + 'mouth-open.png', position: { x: 695, y: 528 } },
+        //@ts-ignore : There is no requirement to write this objects in the constructor, but it will not throw error
+        smile: { src: path + 'mouth-smile.png', position: { x: 653, y: 506 } },
+        sad: { src: path + 'mouth-sad.png', position: { x: 698, y: 531 } },
+    },
+});
+
+setAnimeChanEvents(animeChan);
+
+
 /**@param {String} text */
 const displayError = text => {
     const h1 = document.getElementsByTagName('h1')[0];
