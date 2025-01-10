bad07393e3d3291ac7e3b7902e03ae98ac344183
Dead Paul <dead.every.side@gmail.com>
2025-01-10 21:35:32 +0100

    Added work with falling-leaves module and added some new features with custom-anime-chan

--- js/main.js
+++ js/main.js
@@ -1,30 +1,47 @@
 //@ts-check
-
-import AnimeChan from '../custom-anime-chan/AnimeChan.js'
+import AnimeChan from '../custom-anime-chan/AnimeChan.js';
+import FallingLeaves from '../falling-leaves/FallingLeaves.js'
 
 /** Path for all img sources @type {String}*/
-const path = './img/anime-chan/';
+const pathToAnimeChan = './img/anime-chan/', 
+    /** Path for all img sources @type {String}*/
+    pathToFallingLeaves = './img/falling-leaves/';
 
 /** Anime character @type {AnimeChan}*/
 const animeChan = new AnimeChan('anime-chan-container', {
-    sclera: { main: { src: path + 'sclera.png', position: { x: 0, y: 0 } } },
+    sclera: { main: { src: pathToAnimeChan + 'sclera.png', position: { x: 0, y: 0 } } },
     eyes: {
-        main: { src: path + 'eyes.png', position: { x: 595, y: 368 } },
+        main: { src: pathToAnimeChan + 'eyes.png', position: { x: 595, y: 368 } },
         //@ts-ignore : There is no requirement to write this objects in the constructor, but it will not throw error
-        love: { src: path + 'eyes-love.png', position: { x: 595, y: 368 }},
-        think: { src: path + 'eyes-think.png', position: { x: 595, y: 368 }}
+        love: { src: pathToAnimeChan + 'eyes-love.png', position: { x: 595, y: 368 }},
+        think: { src: pathToAnimeChan + 'eyes-think.png', position: { x: 595, y: 368 }}
     },
-    body: { main: { src: path + 'body.png', position: { x: 0, y: 0 } } },
+    body: { main: { src: pathToAnimeChan + 'body.png', position: { x: 0, y: 0 } } },
     mouth: {
-        main: { src: path + 'mouth-open.png', position: { x: 695, y: 528 } },
+        main: { src: pathToAnimeChan + 'mouth-open.png', position: { x: 695, y: 528 } },
         //@ts-ignore : There is no requirement to write this objects in the constructor, but it will not throw error
-        smile: { src: path + 'mouth-smile.png', position: { x: 653, y: 506 } },
-        sad: { src: path + 'mouth-sad.png', position: { x: 698, y: 531 } },
+        smile: { src: pathToAnimeChan + 'mouth-smile.png', position: { x: 653, y: 506 } },
+        sad: { src: pathToAnimeChan + 'mouth-sad.png', position: { x: 698, y: 531 } },
     },
+    money: {
+        main : {src: pathToAnimeChan + 'money.png', position: { x: 570, y: 790 }}
+    }
 });
+(async () => {
+    (await animeChan.part.money.main).style.display = 'none';
+})();
 
 animeChan.setEvents();
 
+const fallingLeaves = new FallingLeaves('falling-leaves-container', 1, 1, 25, 
+    {height : '3vh', filter : 'opacity(80%)'}, 
+    20, 
+    Array(5).fill('').map((_, index) => `${pathToFallingLeaves}leaf-${index}.png`)
+);
+
+setInterval(() => {
+    fallingLeaves.wind = Math.floor(Math.random() * 2) * 2 - 1;
+}, 50 * 1000);
 
 /**@param {String} text */
 const displayError = text => {
@@ -46,10 +63,14 @@ const displayJarInfo = async function () {
         if (jarInfo) {
             animeChan.setActive('mouth', 'smile');
             animeChan.setActive('eyes', 'love');
-            //@ts-expect-error
+            //@ts-ignore
             document.getElementById('collected-money').textContent = `Собрано: ${jarInfo.balance / 100}`;
-            //@ts-expect-error
+            //@ts-ignore
             document.getElementById('target-money').textContent = `Цель: ${jarInfo.goal / 100}`;
+            /**@type {HTMLImageElement} */
+            const money = await animeChan.part.money.main;
+            money.onclick = () => window.location.href = `https://send.monobank.ua/${jarInfo.sendId}`;
+            money.style.display = 'block';
             return
         }
         else {
